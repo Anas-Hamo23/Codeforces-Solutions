2d5678f7f251c2e4cd977706396ad59632deaf8b
Anas Hamo <104735415+Anas-Hamo23@users.noreply.github.com>
2023-08-27 00:36:39 +0300

    Create Say Hello With C++.cpp

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/Say Hello With C++.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll_a long long
+#define sort_Vector(v) sort(v.begin(),v.end())
+#define sort_Array(a,n) sort(a,a+n)
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+int main() {
+    fast
+    string anas;
+    cin >> anas;
+    cout<<"Hello, "<<anas;
+        return 0;
+}
